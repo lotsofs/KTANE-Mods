cd7603e4950c6e525ee9caaaf368944bb262e3a3
lotsofs <oeneboy@hotmail.com>
2020-05-11 16:38:57 +0200

    Why does the sound not play ingame?

---
+++ TheHeart/Assets/HeartBeat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBeat : MonoBehaviour {
+
+	[SerializeField] KMAudio _bombAudio;
+	[NonSerialized] public bool Beating = false;
+	public float Interval = 1.0f;
+	[SerializeField] float _doubleBeatTime = 0.3f;
+	[SerializeField] float _shrinkRate = 0.03f;
+	[SerializeField] float _firstBeatSize = 1.03f;
+	[SerializeField] float _secondBeatSize = 1.025f;
+
+	Vector3 _baseSize;
+	
+	float _elapsedTime = int.MaxValue;
+	bool _doubleBeatHappened = false;
+
+
+	// Use this for initialization
+	void Start () {
+		_baseSize = transform.localScale;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		_elapsedTime += Time.deltaTime;
+		if (Beating && _elapsedTime > Interval) {
+			_doubleBeatHappened = false;
+			_elapsedTime = 0;
+			transform.localScale = _baseSize * _firstBeatSize;
+			_bombAudio.PlaySoundAtTransform("HeartA", this.transform);
+		}
+		else if (Beating && !_doubleBeatHappened && _elapsedTime > _doubleBeatTime * Interval) {
+			_doubleBeatHappened = true;
+			transform.localScale = _baseSize * _secondBeatSize;
+			_bombAudio.PlaySoundAtTransform("HeartB", this.transform);
+		}
+		else if (transform.localScale.x > _baseSize.x) {
+			transform.localScale *= (1f - (Time.deltaTime * _shrinkRate * Interval));
+		}
+		
+	}
+}
---
+++ TheHeart/Assets/Mod/Scripts/TheHeartModule.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheHeartModule : MonoBehaviour {
+
+	[SerializeField] KMBombInfo _bombInfo;
+	[SerializeField] KMBombModule _bombModule;
+	[SerializeField] BombHelper _bombHelper;
+	[SerializeField] KMAudio _bombAudio;
+
+	[Space]
+
+	[SerializeField] HeartBeat _heartBeat;
+	[SerializeField] KMSelectable _theHeartSelectable;
+
+	float _activationTime;
+
+	// Use this for initialization
+	void Start () {
+		_activationTime = (int)_bombInfo.GetTime();
+		StartModule();
+		_bombModule.OnActivate += ActivateModule;
+	}
+
+	void ActivateModule() {
+		_activationTime = (int)_bombInfo.GetTime();
+		_heartBeat.Beating = true;
+	}
+
+	void StartModule() {
+		_theHeartSelectable.OnInteract += delegate { Defibrilate(); return false; };
+	}
+	
+	void Defibrilate() {
+		_bombHelper.GenericButtonPress(_theHeartSelectable, false, 55);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( Mathf.Abs((int)_bombInfo.GetTime() - _activationTime) >= 60) {
+			Debug.Log("HEEEY");
+		}
+	}
+}
