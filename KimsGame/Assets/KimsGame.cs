52befcd5aecccfe61290ae91ce12074c75495f15
lotsofs <oeneboy@hotmail.com>
2020-08-09 02:00:45 +0200

    Done: Icons are selected
    Todo:
    Rename Icons better
    Make the rest of the module
    PDFify the manual
    Make the SVG

---
+++ KimsGame/Assets/KimsGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KimsGame : MonoBehaviour {
+
+	[SerializeField] BombHelper _bombHelper;
+	[SerializeField] KMBombModule _bombModule;
+
+	[Space]
+
+	[SerializeField] Sprite[] _possibleSprites;
+	[SerializeField] SpriteRenderer[] _displayedSprites;
+
+
+	void Awake() {
+		foreach (SpriteRenderer sprR in _displayedSprites) {
+			sprR.sprite = null;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		_bombModule.OnActivate += PickSprites;
+	}
+
+	void PickSprites() {
+		List<Sprite> spriteList = _possibleSprites.ToList();
+		foreach (SpriteRenderer disR in _displayedSprites) {
+			int index = UnityEngine.Random.Range(0, spriteList.Count);
+			disR.sprite = spriteList[index];
+			spriteList.RemoveAt(index);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ KimsGame/Assets/Mod/Scripts/BombHelper.cs
@@ -0,0 +1,58 @@
+﻿using KModkit;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombHelper : MonoBehaviour {
+
+    [SerializeField] KMBombInfo _bombInfo;
+    [SerializeField] KMAudio _bombAudio;
+
+    [NonSerialized] public int ModuleId;
+    static int _moduleIdCounter = 1;
+
+    /// <summary>
+    /// Increment Module ID
+    /// </summary>
+    void IncrementID() {
+        ModuleId = _moduleIdCounter;
+        _moduleIdCounter++;
+    }
+
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Awake() {
+        IncrementID();
+    }
+
+    /// <summary>
+    /// Generic button press click sound & bomb oomph
+    /// </summary>
+    /// <param name="button"></param>
+    /// <param name="intensityModifier"></param>
+    public void GenericButtonPress(KMSelectable button, bool sound, float intensityModifier = 1) {
+        button.AddInteractionPunch(intensityModifier);
+        if (sound) {
+            _bombAudio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, button.transform);
+        }
+    }
+
+    /// <summary>
+    /// Checks if a digit in the serial number is even (numbers only).
+    /// </summary>
+    /// <param name="digit"></param>
+    /// <returns></returns>
+    public bool IsSerialDigitEven(int digit) {
+        if (digit > 6 || digit < 1) {
+            return false;
+        }
+        char ch = _bombInfo.GetSerialNumber()[digit - 1];
+        if (ch == '0' || ch == '2' || ch == '4' || ch == '6' || ch == '8') {
+            return true;
+        }
+        return false;
+    }
+
+}
